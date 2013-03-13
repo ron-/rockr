cf8ea8d136eb2b935c1e2a704837829ac1ff7fe7
Ron Jones <rrrron@gmail.com>
2013-03-13 18:36:11 -0400

    venue can save

--- spec/models/venue_spec.rb
+++ spec/models/venue_spec.rb
@@ -1,11 +1,23 @@
 require 'spec_helper'
 
-
 describe Venue do
   describe '.new' do
     it 'creates an instance of a venue' do
       venue = Venue.new
       expect(venue).to be_an_instance_of(Venue)
     end
+
+    it 'has name' do
+      venue = Venue.new(name: 'Roseland')
+      expect(venue.name).to eq 'Roseland'
+    end
   end
+
+  describe '.create' do
+    it 'has an id' do
+      venue = Venue.create(name: 'Roseland')
+      expect(venue.id).to_not be nil
+    end
+  end
+
 end
